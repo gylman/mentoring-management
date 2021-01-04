801eff9b46b4e86f6720de2c8be026efe1ee99dd
gylman <gylmanbm@gmail.com>
2021-01-04 17:52:04 +0900

    implement navigation panel options

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,23 +1,40 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NavigationPanel from "./components/NavigationPanel";
+import { Toolbar } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
 
 function Dashboard({ setIsUserLoggedIn }) {
+  const classes = useStyles();
+  const routes = (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/about">
+        <About />
+      </Route>
+    </Switch>
+  );
   return (
-    //everything outside of Switch is will be displayed in every route
-    <>
+    <div className={classes.root}>
       <NavigationPanel setIsUserLoggedIn={setIsUserLoggedIn} />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-      </Switch>
-    </>
+      <main className={classes.content}>
+        <Toolbar />
+        {routes}
+      </main>
+    </div>
   );
 }
 
--- src/components/NavigationPanel.js
+++ src/components/NavigationPanel.js
@@ -15,8 +15,8 @@ import MailIcon from "@material-ui/icons/Mail";
 import { Button } from "@material-ui/core";
 import Logo from "../assets/images/logo.png";
 import { useHistory } from "react-router-dom";
-
-const drawerWidth = 240;
+import cuid from "cuid";
+const drawerWidth = 280;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -57,6 +57,20 @@ const useStyles = makeStyles((theme) => ({
 export default function NavigationPanel({ setIsUserLoggedIn }) {
   const classes = useStyles();
   const history = useHistory();
+  const status = "instructor";
+  let navigationPanelOptions;
+  if (status === "instructor") {
+    navigationPanelOptions = [
+      { name: "Classroom registration" },
+      { name: "Usage information" },
+    ];
+  } else if (status === "administrator") {
+    navigationPanelOptions = [
+      { name: "Classroom registration" },
+      { name: "Usage information" },
+      { name: "Profile" },
+    ];
+  }
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -87,60 +101,28 @@ export default function NavigationPanel({ setIsUserLoggedIn }) {
         <Toolbar />
         <div className={classes.drawerContainer}>
           <List>
-            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-              <ListItem button key={text}>
+            {navigationPanelOptions.map((item, index) => (
+              <ListItem button key={cuid()}>
                 <ListItemIcon>
                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary={item.name} />
               </ListItem>
             ))}
           </List>
           <Divider />
           <List>
-            {["All mail", "Trash", "Spam"].map((text, index) => (
-              <ListItem button key={text}>
+            {[{ name: "Profile" }].map((item, index) => (
+              <ListItem button key={cuid()}>
                 <ListItemIcon>
                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary={item.name} />
               </ListItem>
             ))}
           </List>
         </div>
       </Drawer>
-      <main className={classes.content}>
-        <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </main>
     </div>
   );
 }
