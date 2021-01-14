5ca8ae7a0ff02e29fc1834b8da72d59110b6e992
gylman <gylmanbm@gmail.com>
2021-01-14 16:28:34 +0900

    last version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { Redirect, Route, Switch } from "react-router-dom";
-import SignIn from "./pages/SignIn";
 import { useState } from "react";
 import Dashboard from "./Dashboard";
 import Landing from "./Landing";
